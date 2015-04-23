543b05d401bb7585aee17553a2406fd3b9dc5a5a
AnandPhadke <anand.ph1@gmail.com>
2015-04-23 10:26:38 +0530

    admin and student facility

---
+++ Cafeteria/src/com/cafeteria/Constant.java
@@ -0,0 +1,8 @@
+package com.cafeteria;
+
+public class Constant {
+		public static String EMPLOYEE_NAME= "employee_name";
+		public static String USER_ID= "user_id";
+		public static String IS_ADMIN= "is_admin";
+		
+}
